5822ad39f8ac7bd93e5ba1d594f4c97fdc1ce9f5
vlad-gogov <vlad-gogov@users.noreply.github.com>
2019-04-01 21:59:34 +0300

    Vector

---
+++ gogov_vi/Practice/Practice7/Vector.cpp
@@ -0,0 +1,271 @@
+﻿#include "Vector.h"
+#include <iostream>
+#define M_PI 3.14159265358979323846
+
+Vector::Vector()
+{
+	size = 0;
+	elements = nullptr;
+}
+
+Vector::Vector(const Vector& tmp)
+{
+	size = tmp.size;
+	elements = new double[tmp.size];
+	for (int i = 0; i < tmp.size; i++)
+		elements[i] = tmp.elements[i];
+}
+
+Vector::Vector(int _size)
+{
+	size = _size;
+	elements = new double[size];
+	for (int i = 0; i < size; i++)
+		elements[i] = 0;
+}
+
+Vector::Vector(int _size, double* array)
+{
+	size = _size;
+	elements = new double[size];
+	for (int i = 0; i < size; i++)
+		elements[i] = array[i];
+}
+
+int Vector::getsize()
+{
+	return size;
+}
+
+void Vector::PrintVector()
+{
+	for (int i = 0; i < size; i++)
+		std::cout << elements[i] << " ";
+}
+
+Vector::~Vector()
+{
+	delete[] elements;
+	size = 0;
+}
+
+void Vector::Input()
+{
+	if (size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			std::cout << "Введите " << i + 1 << " элемент: ";
+			std::cin >> elements[i];
+		}
+	}
+}
+
+Vector& Vector::operator+(const Vector & x)
+{
+	if ((x.size != size) || (size < 0) || (x.size < 0))
+	{
+		throw DifferentSizes();
+	}
+	Vector* result = new Vector(size);
+	for (int i = 0; i < size; i++)
+		result->elements[i] = x.elements[i] + elements[i];
+	return *result;
+}
+
+Vector& Vector::operator-(const Vector & x)
+{
+	if ((x.size != size) || (size < 0) || (x.size < 0))
+	{
+		throw DifferentSizes();
+	}
+	Vector* result = new Vector(x.size);
+	for (int i = 0; i < x.size; i++)
+		result->elements[i] = elements[i] - x.elements[i];
+	return *result;
+}
+
+Vector& Vector::operator++()
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i]++;
+	return *result;
+}
+
+Vector& Vector::operator--()
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i]--;
+	return *result;
+}
+
+Vector& Vector::operator--(int)
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i]--;
+	return *result;
+}
+
+Vector& Vector::operator++(int)
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i]++;
+	return *result;
+}
+
+Vector& Vector::operator+()
+{
+	Vector* result = new Vector(*this);
+	return *result;
+}
+
+Vector& Vector::operator-()
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i] *= -1;
+	return *result;
+}
+
+double Vector::operator*(const Vector & x)
+{
+	if ((x.size != size) || (size < 0) || (x.size < 0))
+	{
+		throw DifferentSizes();
+	}
+	double result = 0;
+	for (int i = 0; i < x.size; i++)
+		result += x.elements[i] * elements[i];
+	return result;
+}
+
+Vector& Vector::operator+(double a)
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i] += a;
+	return *result;
+}
+
+
+Vector& Vector::operator*(double a)
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i] *= a;
+	return *result;
+}
+
+Vector& Vector::operator-(double a)
+{
+	Vector* result = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		result->elements[i] -= a;
+	return *result;
+}
+
+Vector& Vector::operator/(double a)
+{
+	Vector* res = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		res->elements[i] = res->elements[i] / a;
+	return *res;
+}
+
+Vector& Vector::operator+=(const Vector & x)
+{
+	Vector* res = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		res->elements[i] = res->elements[i] + x.elements[i];
+	return *res;
+}
+
+Vector& Vector::operator-=(const Vector & x)
+{
+	Vector* res = new Vector(*this);
+	for (int i = 0; i < size; i++)
+		res->elements[i] = res->elements[i] - x.elements[i];
+	return *res;
+}
+
+double Vector::operator*=(const Vector & x)
+{
+	double result = 0;
+	result = (*this) * x;
+	return result;
+}
+
+Vector& Vector::operator=(Vector & x)
+{
+	if ((size == x.size) && (elements == x.elements))
+		return *this;
+	if (size && elements)
+		delete[] elements;
+	size = x.size;
+	elements = new double[size];
+	for (int i = 0; i < size; i++)
+		elements[i] = x.elements[i];
+	return *this;
+}
+
+bool Vector::operator==(const Vector & x)
+{
+	if (size != x.size)
+		return false;
+	for (int i = 0; i < size; i++)
+		if (elements[i] != elements[i])
+			return false;
+	return true;
+}
+
+std::ostream& operator << (std::ostream & s, const Vector & x)
+{
+	for (int i = 0; i < x.size; i++)
+		s << x.elements[i] << " ";
+	s << "\n";
+	return s;
+}
+
+std::istream& operator >> (std::istream & s, Vector & x)
+{
+	for (int i = 0; i < x.size; i++)
+	{
+		std::cout << "Введите " << i + 1 << " элемент: ";
+		s >> x.elements[i];
+	}
+	return s;
+}
+
+double Vector::Len()
+{
+	if (size < 0)
+		throw Sizes();
+	double result = 0;
+	for (int i = 0; i < size; i++)
+		result += elements[i] * elements[i];
+	return sqrt(result);
+}
+
+double Vector::Angle(Vector & x)
+{
+	double lens = x.Len() * Len();
+	double scal = (*this) * x;
+	if (!lens)
+		throw Lens();
+	double result = (acos(scal / lens)) * 180 / M_PI;
+	return result;
+}
+
+void* Vector::operator new(size_t _size)
+{
+	return (void*)(::new unsigned char[_size]);
+}
+
+void Vector::operator delete(void* ptr)
+{
+	delete (Vector*)ptr;
+}
---
+++ gogov_vi/Practice/Practice7/Vector.h
@@ -0,0 +1,59 @@
+﻿#ifndef _VECTOR_H_
+#define _VECTOR_H_
+#include <iostream>
+
+struct Vector
+{
+	int size;
+	double* elements;
+	Vector();
+	int getsize();
+	void PrintVector();
+	void Input();
+	Vector(const Vector& tmp);
+	Vector(int _size);
+	Vector(int _size, double* array);
+	double Len();
+	Vector& operator+(const Vector & x);
+	Vector& operator=(Vector & x);
+	Vector& operator-(const Vector & x);
+	double operator*(const Vector & x);
+	Vector& operator+(double a);
+	Vector& operator*(double a);
+	Vector& operator-(double a);
+	Vector& operator/(double a);
+	Vector& operator++();
+	Vector& operator--();
+	Vector& operator++(int);
+	Vector& operator--(int);
+	Vector& operator+();
+	Vector& operator-();
+	Vector& operator+=(const Vector & x);
+	Vector& operator-=(const Vector & x);
+	double operator*=(const Vector & x);
+	bool operator==(const Vector & x);
+	double Angle(Vector & x);
+	friend std::ostream& operator << (std::ostream &s, const Vector & x);
+	friend std::istream& operator >> (std::istream &s, Vector & x);
+	void* operator new(size_t _size);
+	void operator delete(void* ptr);
+	~Vector();
+};
+
+class DifferentSizes {};
+
+class Sizes {};
+
+class Lens {};
+
+class VectorElemException
+{
+public:
+	int info;
+	VectorElemException(int _info)
+	{
+		info = _info;
+	}
+};
+
+#endif
