43acc3478854ebafeddf81b3f63ae743fe176769
vlad-gogov <vlad-gogov@users.noreply.github.com>
2019-05-27 21:49:03 +0300

    Fix

--- gogov_vi/Practice/Practice10/Container.h
+++ gogov_vi/Practice/Practice10/Container.h
@@ -5,117 +5,124 @@
 template<typename T>
 class Container 
 {
-	T* arr;
-	int count, max_size;
+    T* arr;
+    int count, max_size;
 
-	void malloc();
+    void malloc();
 public:
-	Container();
-	Container(const Container&);
-	~Container();
-	bool is_full() const;
-	bool is_empty() const;
-	int find_elem(const T) const;
-	void add_elem(const T);
-	void delete_elem(const T);
-	T& operator[](int) const;
-	void print() const;
+    Container();
+    Container(const Container&);
+    ~Container();
+    bool is_full() const;
+    bool is_empty() const;
+    int find_elem(const T) const;
+    void add_elem(const T);
+    void delete_elem(const T);
+    T& operator[](int);
+    const T& operator[](int) const;
+    void print() const;
 };
 
 template<typename T>
 Container<T>::Container()
 {
-	count = max_size = 0;
-	arr = nullptr;
+    count = max_size = 0;
+    arr = nullptr;
 }
 
 template<typename T>
 void Container<T>::malloc()
 {
-	max_size += 10;
-	T* new_arr = new T[max_size];
-	for (int i = 0; i < count; i++)
-		new_arr[i] = arr[i];
-	if (arr)
-		delete[] arr;
-	arr = new_arr;
+    max_size += 10;
+    T* new_arr = new T[max_size];
+    memcpy(new_arr, arr, sizeof(T) * count);
+    if (arr)
+        delete[] arr;
+    arr = new_arr;
 
 }
 
 template<typename T>
 Container<T>::Container(const Container& x)
 {
-	count = x.count;
-	max_size = x.max_size;
-	arr = new T[max_size];
-	for (int i = 0; i < count; i++)
-		arr[i] = x.arr[i];
+    count = x.count;
+    max_size = x.max_size;
+    arr = new T[max_size];
+    memcpy(arr, x.arr, sizeof(T) * count);
 }
 
 template<typename T>
 Container<T>::~Container()
 {
-	delete[] arr;
+    delete[] arr;
 }
 
 template<typename T>
 bool Container<T>::is_full() const
 {
-	return (count == max_size) ? 1 : 0;
+    return (count == max_size) ? 1 : 0;
 }
 
 template<typename T>
 bool Container<T>::is_empty() const
 {
-	return (count == 0) ? 1 : 0;
+    return (count == 0) ? 1 : 0;
 }
 
 template<typename T>
-T& Container<T>::operator[](int idx) const
+T& Container<T>::operator[](int idx)
 {
-	if (idx >= count)
-		throw std::out_of_range("Выход за границы.\n");
-	return arr[idx];
+    if (idx >= count)
+        throw std::out_of_range("Выход за границы.\n");
+    return arr[idx];
+}
+
+template<typename T>
+const T& Container<T>::operator[](int idx) const
+{
+    if (idx >= count)
+        throw std::out_of_range("Выход за границы.\n");
+    return arr[idx];
 }
 
 template<typename T>
 int Container<T>::find_elem(const T x) const
 {
-	for (int i = 0; i < count; i++)
-		if (x == arr[i])
-			return i;
-	return -1;
+    for (int i = 0; i < count; i++)
+        if (x == arr[i])
+            return i;
+    return -1;
 }
 
 template<typename T>
 void Container<T>::add_elem(const T x)
 {
-	if (is_full())
-		malloc();
-	arr[count] = x;
-	count++;
+    if (is_full())
+        malloc();
+    arr[count] = x;
+    count++;
 }
 
 template<typename T>
 void Container<T>::delete_elem(const T x)
 {
-	int idx = find_elem(x);
-	if (idx == -1)
-	{
-		throw "Элемент не найден \n";
-	}
-	T tmp = arr[idx];
-	arr[idx] = arr[count - 1];
-	arr[count - 1] = tmp;
-	count--;
+    int idx = find_elem(x);
+    if (idx == -1)
+    {
+        throw "Элемент не найден \n";
+    }
+    T tmp = arr[idx];
+    arr[idx] = arr[count - 1];
+    arr[count - 1] = tmp;
+    count--;
 }
 
 template<typename T>
 void Container<T>::print() const
 {
-	for (int i = 0; i < count; i++)
-		std::cout << arr[i] << " ";
-	std::cout << "\n";
+    for (int i = 0; i < count; i++)
+        std::cout << arr[i] << " ";
+    std::cout << "\n";
 }
 
 
@@ -132,120 +139,129 @@ void Container<T>::print() const
 template<typename T>
 class Container<T*>
 {
-	T** arr;
-	int count, max_size;
+    T** arr;
+    int count, max_size;
 
-	void malloc();
+    void malloc();
 public:
-	Container<T*>();
-	Container<T*>(const Container&);
-	~Container<T*>();
-	bool is_full() const;
-	bool is_empty() const;
-	int find_elem(const T) const;
-	void add_elem(const T);
-	void delete_elem(const T);
-	T*& operator[](int) const;
-	void print() const;
+    Container();
+    Container(const Container&);
+    ~Container();
+    bool is_full() const;
+    bool is_empty() const;
+    int find_elem(const T*) const;
+    void add_elem(const T*);
+    void delete_elem(const T*);
+    T*& operator[](int);
+    const T*& operator[](int) const;
+    void print() const;
 };
 
 template<typename T>
 Container<T*>::Container()
 {
-	count = max_size = 0;
-	arr = nullptr;
+    count = max_size = 0;
+    arr = nullptr;
 }
 
 template<typename T>
 void Container<T*>::malloc()
 {
-	max_size += 10;
-	T** new_arr = new T*[max_size];
-	for (int i = 0; i < count; i++)
-		new_arr[i] = new T(**(arr + i));
-	if (arr)
-		delete[] arr;
-	arr = new_arr;
+    max_size += 10;
+    T** new_arr = new T*[max_size];
+    for (int i = 0; i < count; i++)
+        new_arr[i] = new T(**(arr + i));
+    if (arr)
+        delete[] arr;
+    arr = new_arr;
 }
 
 template<typename T>
-void Container<T*>::add_elem(const T x)
+void Container<T*>::add_elem(const T* x)
 {
-	if (is_full())
-		malloc();
-	arr[count] = new T(x);
-	count++;
+    if (is_full())
+        malloc();
+    arr[count] = new T(*x);
+    count++;
 }
 
 template<typename T>
 Container<T*>::Container(const Container& x)
 {
-	count = x.count;
-	max_size = x.max_size;
-	arr = new T*[x.max_size];
-	for (int i = 0; i < count; i++)
-	{
-		arr[i] = new T(**(x.arr + i));
-	}
+    count = x.count;
+    max_size = x.max_size;
+    arr = new T*[x.max_size];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = new T(**(x.arr + i));
+    }
 }
 
 template<typename T>
 Container<T*>::~Container()
 {
-	for (int i = 0; i < count; i++)
-		delete arr[i];
-	delete arr;
+    for (int i = 0; i < count; i++)
+        delete arr[i];
+    delete arr;
 }
 
 template<typename T>
 bool Container<T*>::is_full() const
 {
-	return (count == max_size) ? 1 : 0;
+    return (count == max_size) ? 1 : 0;
 }
 
 template<typename T>
 bool Container<T*>::is_empty() const
 {
-	return (count == 0) ? 1 : 0;
+    return (count == 0) ? 1 : 0;
 }
 
 template<typename T>
-T*& Container<T*>::operator[](int idx) const
+T*& Container<T*>::operator[](int idx)
 {
-	if (idx >= count)
-		throw std::out_of_range("Выход за границы.\n");
-	return arr[idx];
+    if (idx >= count)
+        throw std::out_of_range("Выход за границы.\n");
+    return arr[idx];
 }
 
 template<typename T>
-int Container<T*>::find_elem(const T x) const
+const T*& Container<T*>::operator[](int idx) const
 {
-	for (int i = 0; i < count; i++)
-		if (x == **(arr + i))
-			return i;
-	return -1;
+    if (idx >= count)
+        throw std::out_of_range("Выход за границы.\n");
+    return arr[idx];
 }
 
 template<typename T>
-void Container<T*>::delete_elem(const T x)
+int Container<T*>::find_elem(const T* x) const
 {
-	int idx = find_elem(x);
-	if (idx == -1)
-	{
-		throw "Элемент не найден. \n";
-	}
-	delete arr[idx];
-	arr[idx] = arr[count - 1];
-	count--;
+    for (int i = 0; i < count; i++)
+        if (*x == **(arr + i))
+            return i;
+    return -1;
+}
+
+template<typename T>
+void Container<T*>::delete_elem(const T* x)
+{
+    int idx = find_elem(x);
+    if (idx == -1)
+    {
+        throw "Элемент не найден. \n";
+    }
+    delete arr[idx];
+    arr[idx] = arr[count - 1];
+    count--;
 
 }
 
 template<typename T>
 void Container<T*>::print() const
 {
-	for (int i = 0; i < count; i++)
-		std::cout << *(arr[i]) << " ";
-	std::cout << "\n";
+    for (int i = 0; i < count; i++)
+        std::cout << *(arr[i]) << " ";
+    std::cout << "\n";
 }
 
 #endif 
--- gogov_vi/Practice/Practice10/main.cpp
+++ gogov_vi/Practice/Practice10/main.cpp
@@ -4,158 +4,158 @@
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	Container<double> a;
-	double* tmp = new double[10];
-	std::cout << "Добавление 10 элементов: ";
-	for (int i = 0; i < 10; i++)
-	{
-		tmp[i] = i;
-		a.add_elem(i);
-	}
-	a.print();
-	std::cout << "Копирование из a в b: ";
-	Container<double> b(a);
-	b.print();
-	std::cout << "Добавление 11 элемента: ";
-	a.add_elem(1);
-	a.print();
+    setlocale(LC_ALL, "Russian");
+    Container<double> a;
+    double* tmp = new double[10];
+    std::cout << "Добавление 10 элементов: ";
+    for (int i = 0; i < 10; i++)
+    {
+        tmp[i] = i;
+        a.add_elem(i);
+    }
+    a.print();
+    std::cout << "Копирование из a в b: ";
+    Container<double> b(a);
+    b.print();
+    std::cout << "Добавление 11 элемента: ";
+    a.add_elem(1);
+    a.print();
 
-	std::cout << "a[5] = ";
-	try
-	{
-		std::cout << a[5] << "\n";
-	}
-	catch (std::out_of_range e)
-	{
-		std::cout << e.what();
-	}
+    std::cout << "a[5] = ";
+    try
+    {
+        std::cout << a[5] << "\n";
+    }
+    catch (std::out_of_range e)
+    {
+        std::cout << e.what();
+    }
 
-	std::cout << "a[10] = ";
-	try
-	{
-		std::cout << a[10] << "\n";
-	}
-	catch (std::out_of_range e)
-	{
-		std::cout << e.what();
-	}
+    std::cout << "a[10] = ";
+    try
+    {
+        std::cout << a[10] << "\n";
+    }
+    catch (std::out_of_range e)
+    {
+        std::cout << e.what();
+    }
 
-	std::cout << "Изменение a[7] = ";
-	try
-	{
-		a[7] = 5;
-		std::cout << a[7] << "\n";
-	}
-	catch (std::out_of_range e)
-	{
-		std::cout << e.what();
-	}
+    std::cout << "Изменение a[7] = ";
+    try
+    {
+        a[7] = 5;
+        std::cout << a[7] << "\n";
+    }
+    catch (std::out_of_range e)
+    {
+        std::cout << e.what();
+    }
 
-	std::cout << "Индекс элемента равный 5: ";
-	int idx = a.find_elem(5);
-	if (idx == -1)
-		std::cout << "Элемент не найден." << "\n";
-	std::cout << idx << "\n";
-	std::cout << "Индекс элемента равный 6: ";
-	idx = a.find_elem(6);
-	if (idx == -1)
-		std::cout << "Элемент не найден." << "\n";
-	a.print();
-	std::cout << "Удаление элемента равный 5: ";
-	try 
-	{
-		a.delete_elem(5);
-	}
-	catch (const char* k)
-	{
-		std::cout << k;
-	}
-	a.print();
-	std::cout << "Удаление элемента равный 7: ";
-	try
-	{
-		a.delete_elem(7);
-	}
-	catch (const char* k)
-	{
-		std::cout << k;
-	}
-	a.print();
+    std::cout << "Индекс элемента равный 5: ";
+    int idx = a.find_elem(5);
+    if (idx == -1)
+        std::cout << "Элемент не найден." << "\n";
+    std::cout << idx << "\n";
+    std::cout << "Индекс элемента равный 6: ";
+    idx = a.find_elem(6);
+    if (idx == -1)
+        std::cout << "Элемент не найден." << "\n";
+    a.print();
+    std::cout << "Удаление элемента равный 5: ";
+    try 
+    {
+        a.delete_elem(5);
+    }
+    catch (const char* k)
+    {
+        std::cout << k;
+    }
+    a.print();
+    std::cout << "Удаление элемента равный 7: ";
+    try
+    {
+        a.delete_elem(7);
+    }
+    catch (const char* k)
+    {
+        std::cout << k;
+    }
+    a.print();
 
-	std::cout << "\nМассив указателей. \n";
+    std::cout << "\nМассив указателей. \n";
 
-	Container<double*> c;
-	for (int i = 0; i < 10; i++)
-		c.add_elem(tmp[i]);
-	c.print();
-	std::cout << "Копирование из c в d: ";
-	Container<double*> d(c);
-	d.print();
-	std::cout << "Добавление 11 элемента: ";
-	c.add_elem(1);
-	c.print();
+    Container<double*> c;
+    for (int i = 0; i < 10; i++)
+        c.add_elem(tmp + i);
+    c.print();
+    std::cout << "Копирование из c в d: ";
+    Container<double*> d(c);
+    d.print();
+    std::cout << "Добавление 11 элемента: ";
+    c.add_elem(tmp + 1);
+    c.print();
 
 
-	std::cout << "a[5] = ";
-	try
-	{
-		std::cout << *(c[5]) << "\n";
-	}
-	catch (std::out_of_range e)
-	{
-		std::cout << e.what();
-	}
+    std::cout << "a[5] = ";
+    try
+    {
+        std::cout << *(c[5]) << "\n";
+    }
+    catch (std::out_of_range e)
+    {
+        std::cout << e.what();
+    }
 
-	std::cout << "a[10] = ";
-	try
-	{
-		std::cout << *(c[10]) << "\n";
-	}
-	catch (std::out_of_range e)
-	{
-		std::cout << e.what();
-	}
+    std::cout << "a[10] = ";
+    try
+    {
+        std::cout << *(c[10]) << "\n";
+    }
+    catch (std::out_of_range e)
+    {
+        std::cout << e.what();
+    }
 
-	std::cout << "Изменение a[7] = ";
-	try
-	{
-		c[7] = new double(5);
-		std::cout << *(c[7]) << "\n";
-	}
-	catch (std::out_of_range e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << "Индекс элемента равный 5: ";
-	idx = c.find_elem(5);
-	if (idx == -1)
-		std::cout << "Элемент не найден." << "\n";
-	std::cout << idx << "\n";
-	std::cout << "Индекс элемента равный 6: ";
-	idx = c.find_elem(6);
-	std::cout << idx << "\n";
-	if (idx == -1)
-		std::cout << "Элемент не найден." << "\n";
-	std::cout << "Удаление элемента равный 5: ";
-	try
-	{
-		c.delete_elem(5);
-	}
-	catch (const char* k)
-	{
-		std::cout << k;
-	}
-	c.print();
-	std::cout << "Удаление элемента равный 7: ";
-	try
-	{
-		a.delete_elem(7);
-	}
-	catch (const char* k)
-	{
-		std::cout << k;
-	}
-	c.print();
-	return 0;
+    std::cout << "Изменение a[7] = ";
+    try
+    {
+        c[7] = new double(5);
+        std::cout << *(c[7]) << "\n";
+    }
+    catch (std::out_of_range e)
+    {
+        std::cout << e.what();
+    }
+    std::cout << "Индекс элемента равный 5: ";
+    idx = c.find_elem(tmp + 5);
+    if (idx == -1)
+        std::cout << "Элемент не найден." << "\n";
+    std::cout << idx << "\n";
+    std::cout << "Индекс элемента равный 6: ";
+    idx = c.find_elem(tmp + 6);
+    std::cout << idx << "\n";
+    if (idx == -1)
+        std::cout << "Элемент не найден." << "\n";
+    std::cout << "Удаление элемента равный 5: ";
+    try
+    {
+        c.delete_elem(tmp + 5);
+    }
+    catch (const char* k)
+    {
+        std::cout << k;
+    }
+    c.print();
+    std::cout << "Удаление элемента равный 7: ";
+    try
+    {
+        a.delete_elem(7);
+    }
+    catch (const char* k)
+    {
+        std::cout << k;
+    }
+    c.print();
+    return 0;
 }
